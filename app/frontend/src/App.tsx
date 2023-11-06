ba62f1bfac76afcddcb6f48be6e889aeaa4c7b02
Tai Nguyen <tai.nguyen@wartsila.com>
2023-11-06 23:06:34 +0200

    Initial Commit

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -8,13 +8,6 @@ function App() {
       <header className="App-header">
         Document Chat Application
       </header>
-      <body>
-        <br />
-        <label htmlFor="input">Name: </label>
-        <input type="text" onClick={(e) => {
-          console.log(e.target)
-        }} />
-      </body>
     </div>
   );
 }
