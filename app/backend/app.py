37c5d71997e2159c6eef17068755a5eb9ecee121
Tai Nguyen <tai.nguyen@wartsila.com>
2023-11-07 12:55:06 +0200

    Initial commit

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify
 import dotenv, requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/", defaults={"path" : "index.html"})
-def hello():
-    return app.send_static_file("index.html")
+@app.route('/')
+def hello_world():
+    response = {
+        'message' : 'hello world from backend'
+    }
+    return jsonify(response)
+
+@app.route('/test')
+def test():
+    return 'testing from a different route'
 
 if __name__ == "__main__":
     app.run(debug=True)
