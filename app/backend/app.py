e7fa6664eeb3c2868bc35b34b28f1f41820c2d0a
TaiNguyen2407 <tai.nguyen@wartsila.com>
2023-11-08 15:27:42 +0200

    Feat clean up backend

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import dotenv, requests
 from flask_cors import CORS
-import openai
+from gpt4all import GPT4All
 
 app = Flask(__name__)
 CORS(app)
@@ -12,15 +12,6 @@ def hello_world():
         'message' : 'hello world from backend'
     }
     return jsonify(response)
-
-
-from gpt4all import GPT4All
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-with model.chat_session():
-    response1 = model.generate(prompt='hello', temp=0)
-    response2 = model.generate(prompt='who is president of united states', temp=0)
-    response3 = model.generate(prompt='who is the president before', temp=0)
-    print(model.current_chat_session)
 
 if __name__ == "__main__":
     app.run(debug=True)
