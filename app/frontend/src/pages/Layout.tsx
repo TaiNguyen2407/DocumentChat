202906af20f1388a027e4ba62a1511c4276bfe10
Anish Maharjan <anish.maharjan@tgr-wrt.com>
2023-11-18 16:45:16 +0200

    added logo in side nav bar

--- app/frontend/src/pages/Layout.tsx
+++ app/frontend/src/pages/Layout.tsx
@@ -1,4 +1,8 @@
-import { faBars, faCircleInfo, faMessage } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCircleInfo,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon as HamburgerMenuIcon } from "@fortawesome/react-fontawesome";
 import { FontAwesomeIcon as ChatIcon } from "@fortawesome/react-fontawesome";
 import { FontAwesomeIcon as InfoIcon } from "@fortawesome/react-fontawesome";
@@ -7,33 +11,41 @@ import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-    const collapseHamburgerMenu = () => {
-        setIsCollapsed(!isCollapsed);
-    }
-    return (
-        <div className="flex flex-row w-full h-full">
-            <Sidebar collapsed={isCollapsed} className="border shadow-lg">
-                {!isCollapsed ? (
-                    <h1 className="text-2xl font-extrabold py-4 pl-4 border-b shadow-lg ease-linear delay-750">Document Chat</h1>
-                ) : (<></>)
-                }
-                <Menu>
-                    <MenuItem icon={<HamburgerMenuIcon icon={faBars} />} onClick={collapseHamburgerMenu}  />
-                    <MenuItem icon={<ChatIcon icon={faMessage} /> } component={<Link to='/'/>}> 
-                        Chat
-                    </MenuItem>
-                    <MenuItem icon={<InfoIcon icon={faCircleInfo} />} component={<Link to='/info' />}> 
-                        Info
-                    </MenuItem>
-                </Menu>
-            </Sidebar>
-            <div className="flex flex-col w-full h-full bg-gradient-to-r from-gray-200 to-gray-100">
-                <Outlet />
-            </div>
+  const collapseHamburgerMenu = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+  return (
+    <div className="flex flex-row w-full h-full">
+      <Sidebar collapsed={isCollapsed} className="border shadow-lg">
+        <div>
+          <img src={require("../resources/images/logo.png")} alt="logo" />
         </div>
-    )
-}
+        <Menu>
+          <MenuItem
+            icon={<HamburgerMenuIcon icon={faBars} />}
+            onClick={collapseHamburgerMenu}
+          />
+          <MenuItem
+            icon={<ChatIcon icon={faMessage} />}
+            component={<Link to="/" />}
+          >
+            Chat
+          </MenuItem>
+          <MenuItem
+            icon={<InfoIcon icon={faCircleInfo} />}
+            component={<Link to="/info" />}
+          >
+            Info
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+      <div className="flex flex-col w-full h-full bg-gradient-to-r from-gray-200 to-gray-100">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
