6a82d1c45094be20408d30660175f22234fcb1f3
Anish Maharjan <anish.maharjan@tgr-wrt.com>
2023-11-24 19:30:28 +0200

    Revert "fix/Users/anishmaharjan/Documents/Metropolia/MobileProject/Project/DocumentChat/app/frontend/git statusdded local server info for github actions"
    This reverts commit b3d577ec910dfc85649c1ad2b5c7807b6e1e5efb.

--- app/frontend/playwright.config.ts
+++ app/frontend/playwright.config.ts
@@ -74,8 +74,4 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-  },
 });
