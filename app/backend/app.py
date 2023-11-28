3c409128e8a9242f1dc46865462f04d56698ae63
TaiNguyen2407 <tai.nguyen@wartsila.com>
2023-11-28 22:19:44 +0200

    Feat(backend): clean up backend app.py, add some more functions in utils.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,31 +1,38 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
 from gpt4all import GPT4All
 from langchain.document_loaders import PyPDFLoader
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.embeddings import LlamaCppEmbeddings
-from langchain.text_splitter import  RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.llms import LlamaCpp
-from logic.utils import generate_answer_from_chat_model, split_chunks, similarity_search, create_index, save_index_to_local_folder, load_index_from_local
+from logic.utils import generate_answer_from_chat_model, split_chunks, similarity_search, create_index, replace_folder_if_exists, is_file_size_valid
 
 
 app = Flask(__name__)
 CORS(app)
 
 data = []
+app.config['UPLOAD_FOLDER'] = './docs'
+MAX_FILE_SIZE= app.config['MAX_CONTENT_LENGTH'] = 1 * 1000 * 1000 # max file size is around 1mb
+ALLOWED_EXTENSIONS = 'pdf'
+local_index_folder = 'document_index'
+
+llama_2_7b_chat_path = 'models/llama-2-7b-chat.ggmlv3.q2_K.bin'
+embeddings = LlamaCppEmbeddings(model_path=llama_2_7b_chat_path)
 
 chat_model = "orca-mini-3b-gguf2-q4_0.gguf"
 model = GPT4All(model_name=chat_model)
+
 
 @app.route('/')
 def chat():
     return "hello world"
 
-    
 
 @app.route('/api/chat/all-messages', methods=["GET"])
 def chat_history():
@@ -45,55 +52,80 @@ def handle_frontend_request():
     return jsonify(data_to_send_back)
 
 
-llama_2_7b_chat_path = 'models/llama-2-7b-chat.ggmlv3.q2_K.bin'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.split('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler])
-loader = PyPDFLoader('path to pdf file')
-embeddings = LlamaCppEmbeddings(model_path=llama_2_7b_chat_path)
-llm = LlamaCpp(
-    model_path=llama_2_7b_chat_path, 
-    input={
-        "temperature": 0,
-        "max_length": 2048,
-        "top_p": 1
-    },
-    n_ctx=2048,
-    callback_manager=callback_manager,
-    verbose=False
-)
 
-# Create Index
-docs = loader.load()
-chunks = split_chunks(docs)
-index = create_index(chunks)
+def create_index_for_uploaded_document(file) -> FAISS:
+    loader = PyPDFLoader(app.config['UPLOAD_FOLDER'] + '/' + file.filename)
+    docs = loader.load()
+    chunks = split_chunks(docs)
+    index = create_index(chunks, embeddings)
+    return index
 
-# Save Index (use this to save the index for later use)
-# Comment the line below after running once successfully (IMPORTANT)
 
-#save_index_to_local_folder(index, 'path to index folder')
-index.save_local('text_index')
+def save_index_to_local_folder(index: FAISS, local_index_folder_path: str):
+    replace_folder_if_exists(local_index_folder_path)
+    index.save_local(folder_path=local_index_folder_path)
 
-index = FAISS.load_local("./text_index", embeddings)
 
-# Set your query here manually
-question = "Summarize the article"
-matched_docs, sources = similarity_search(question, index)
+@app.route('/api/upload/upload-document', methods=["POST"])
+def handle_upload_file():
+    print(request.files)
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if not is_file_size_valid(file_path, MAX_FILE_SIZE):
+        return 'File size exceeds the allowed limit'
+
+    if file and allowed_file(file.filename): 
+        file.save(app.config['UPLOAD_FOLDER'] + '/' + file.filename)
+        index = create_index_for_uploaded_document(file)
+        save_index_to_local_folder(index, local_index_folder_path=local_index_folder)
+        return 'File uploaded successfully'
+
+    return 'File not allowed'
+    
+
+def init_llama_model():
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler])
+    llm = LlamaCpp(
+        model_path=llama_2_7b_chat_path, 
+        input={
+            "temperature": 0,
+            "max_length": 2048,
+            "top_p": 1
+        },
+        n_ctx=2048,
+        callback_manager=callback_manager,
+        verbose=False
+    )   
+    return llm
 
 template = """
 Please use the following context to answer questions.
 Context: {context}
 ---
 Question: {question}
-Answer: Let's think step by step."""
+Answer: Let's think step by step.
+"""
 
-context = "\n".join([doc.page_content for doc in matched_docs])
-prompt = PromptTemplate(template=template, input_variables=["context", "question"]).partial(context=context)
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+question = "what is the article about ?"
 
-print(llm_chain.run(question))
-
-
-
+def chat_with_document():
+    loaded_index = FAISS.load_local(f'./{local_index_folder}', embeddings)
+    matched_docs = similarity_search(question, loaded_index)
+    context = "\n".join([doc.page_content for doc in matched_docs])
+    prompt = PromptTemplate(template=template, input_variables=["context", "question"]).partial(context=context)
+    llm_chain = LLMChain(prompt=prompt, llm=init_llama_model())
+    print(llm_chain.run(question))
 
 
 if __name__ == "__main__":
--- app/backend/logic/utils.py
+++ app/backend/logic/utils.py
@@ -1,8 +1,33 @@
 from typing import List
+import os
 from langchain.text_splitter import  RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import LlamaCppEmbeddings
 from gpt4all import GPT4All
+
+
+def replace_folder_if_exists(folder_path):
+    if os.path.exists(folder_path):
+        # If the folder already exists, remove it
+        for root, dirs, files in os.walk(folder_path, topdown=False):
+            for file in files:
+                os.remove(os.path.join(root, file))
+            for dir in dirs:
+                os.rmdir(os.path.join(root, dir))
+        os.rmdir(folder_path)
+
+    # Create a new folder
+    os.makedirs(folder_path)
+
+def is_file_size_valid(file_path, max_size: int):
+    return os.path.getsize(file_path) <= max_size
+
+def generate_answer_from_chat_model(model: GPT4All, question: str, conversation_history: list, temp: int = 0):
+    context = " ".join([msg["content"] for msg in conversation_history])
+
+    with model.chat_session():
+        return model.generate(prompt=f"{context}\nUser: {question}", temp=temp)
+
 
 def split_chunks(sources: List[str]):
     chunks = []
@@ -32,16 +57,3 @@ def similarity_search(query: str, index: FAISS):
         )
 
     return matched_docs, sources
-
-def save_index_to_local_folder(index: FAISS, local_folder_path: str):
-    return index.save_local(folder_path=local_folder_path)
-
-def load_index_from_local(index: FAISS, embeddings: LlamaCppEmbeddings, local_folder_path: str):
-    return index.load_local(folder_path=local_folder_path, embeddings=embeddings)
-    
-
-def generate_answer_from_chat_model(model: GPT4All, question: str, conversation_history: list, temp: int = 0):
-    context = " ".join([msg["content"] for msg in conversation_history])
-
-    with model.chat_session():
-        return model.generate(prompt=f"{context}\nUser: {question}", temp=temp)
